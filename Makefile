d05610a21861f340735551f7892d69ce2267e974
Riley Fischer <rfmineguy@gmail.com>
2024-07-16 02:50:25 -0700

    Build system and simple driver

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 OUT_DIR := out
+GETOPT_DIR := getopt
 SRC_DIR := src
-SOURCES := $(SRC_DIR)/main.c
+INCLUDE_DIR := include
+SOURCES := $(SRC_DIR)/*.c
 
 BIN := projman
 
 .PHONY: always build
+.PHONY: getopt
 always:
 	mkdir -p $(OUT_DIR)
-build: always $(OUT_DIR)/$(BIN)
+build: always getopt $(OUT_DIR)/$(BIN)
+getopt:
+	gengetopt -i config.ggo --src-output-dir $(SRC_DIR) --header-output-dir $(INCLUDE_DIR)/projman  
 #=============================
 
 $(OUT_DIR)/$(BIN): $(SOURCES)
-	gcc $^ -o $@
+	gcc $^ -o $@ -I$(INCLUDE_DIR)/projman
--- src/main.c
+++ src/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include "cmdline.h"
 
-int main() {
-	printf("Hello World\n");
+struct register_info {
+	const char *projname, *projpath;
+};
+
+int command_run_list() {}
+int command_run_register(struct register_info info) {}
+
+int main(int argc, char** argv) {
+	struct gengetopt_args_info info = {0};
+	if (cmdline_parser(argc, argv, &info) != 0) {
+		fprintf(stderr, "Failed to parse arguments\n");
+		return 1;
+	}
+	if (info.list_given) {
+		command_run_list();
+	}
+	if (info.register_given) {
+		struct register_info rinfo = (struct register_info){0};
+		rinfo.projname = info.name_arg;
+		rinfo.projpath = info.path_arg;
+		command_run_register(rinfo);
+	}
 }
